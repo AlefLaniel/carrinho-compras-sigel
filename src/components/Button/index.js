09d7e2d9fb874ed72a54e1397c9a4ab03745d679
AlefLaniel <alef6940@gmail.com>
2024-07-11 08:12:15 -0300

    update: implementando tailwind

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-import * as C from "./styles";
 
 const Button = ({ Text, onClick, Type = "button" }) => {
   return (
-    <C.Button type={Type} onClick={onClick}>
+    <button className="py-4 px-5 cursor-pointer rounded-md w-full bg-blue-600 text-white font-semibold text-base max-w-[350px]" type={Type} onClick={onClick}>
       {Text}
-    </C.Button>
+    </button>
   );
 };
 
