178de74eaa4c1304d0ecebb6f9543bd3d13201fd
AlefLaniel <alef6940@gmail.com>
2024-08-15 09:55:13 -0300

    update: estilizando o checkout da compra

--- src/pages/CheckShopping/index.jsx
+++ src/pages/CheckShopping/index.jsx
@@ -9,13 +9,27 @@ const CheckShopping = () => {
   console.log('dados do carrinho: ', cartItems);
 
   return (
-    <div>
-      <div>
+    <div className="w-max[800px] my-0 mx-auto p-5 bg-[#f8f8f8] rounded-lg">
+      <h2 className='text-center mb-5'>Resumo da Compra</h2>
+      <div className="border-t border-b border-[#ddd]">
         {cartItems.map((item) => (
-          <h3 key={item.id}>{item.title}</h3>
+          <div key={item.id} className="flex py-4 px-0 border-b border-[#ddd]">
+            <img src={item.thumbnail} alt={item.title} className="w-24 h-24 object-cover rounded-lg mr-5"  />
+            <div className="flex-1">
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+              <div className="flex justify-between">
+                <p>Preço unitário: {formatCurrency(item.price, 'BRL')}</p>
+                <p>Quantidade: {item.quantity}</p>
+              </div>
+            </div>
+            <div className="text-base font-bold text-[#333]">
+              <h4>Total: {formatCurrency(item.price * item.quantity, 'BRL')}</h4>
+            </div>
+          </div>
         ))}
       </div>
-      <h4>Preço total: {formatCurrency(totalPrice, 'BRL')}</h4>
+      <h3 className="text-right text-lg mt-5">Preço total: {formatCurrency(totalPrice, 'BRL')}</h3>
     </div>
   );
 };
