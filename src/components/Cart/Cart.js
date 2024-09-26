99437013503c240c69d448f92d37a4d5c8942f4f
AlefLaniel <alef6940@gmail.com>
2024-09-26 14:52:10 -0300

    update: melhorando a area do checkout

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import CartItem from '../CartItem/CartItem';
 import AppContext from '../../contexts/AppContext';
 import formatCurrency from '../../utils/formatCurrency';
@@ -8,9 +8,11 @@ function Cart() {
   const history = useNavigate();
   const { cartItems, isCartVisible, totalPrice, setTotalPrice } = useContext(AppContext);
 
-  console.log(cartItems);
-
-  setTotalPrice(cartItems.reduce((acc, item) => item.price + acc, 0));
+  // Use efeito para recalcular o total sempre que o carrinho mudar
+  useEffect(() => {
+    const updatedTotalPrice = cartItems.reduce((acc, item) => acc + (item.price * item.available_quantity), 0);
+    setTotalPrice(updatedTotalPrice);
+  }, [cartItems, setTotalPrice]); // Recalcula quando cartItems ou setTotalPrice mudarem
 
   return (
     <section className={`w-full max-w-[330px] bg-white h-screen fixed top-0 right-0 p-[100px_20px_20px] flex flex-col justify-between transform transition-all duration-400 ease ${isCartVisible ? 'translate-x-0' : 'translate-x-full'}`}>
@@ -23,7 +25,13 @@ function Cart() {
       <div className="text-[1.8rem] font-medium py-[35px_0_15px] border-t border-gray-300">
         {formatCurrency(totalPrice, 'BRL')}
       </div>
-      <button className="w-full p-3 bg-green-500 font-semibold text-lg text-white rounded-md" onClick={() => { history('/finalizacompra'); }}>Finalizar</button>
+      
+      <button
+        className="w-full p-3 bg-green-500 font-semibold text-lg text-white rounded-md"
+        onClick={() => { history('/finalizacompra'); }}
+      >
+        Finalizar
+      </button>
     </section>
   );
 }
--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import propTypes from 'prop-types';
-import { BsCartDashFill } from 'react-icons/bs';
+import {  BsCartDashFill, BsDashCircleFill, BsPlusCircleFill } from 'react-icons/bs';
 
 import formatCurrency from '../../utils/formatCurrency';
 import AppContext from '../../contexts/AppContext';
@@ -8,10 +8,33 @@ import AppContext from '../../contexts/AppContext';
 function CartItem({ data }) {
 
   const { cartItems, setCartItems } = useContext(AppContext);
-  const { id, thumbnail, title, price } = data;
+  const { id, thumbnail, title, price, available_quantity  } = data;
 
   const handleRemoveItem = () => {
     const updatedItems = cartItems.filter((item) => item.id !== id);
+    setCartItems(updatedItems);
+  };
+
+  const handleIncreaseQuantity = () => {
+    const updatedItems = cartItems.map((item) => {
+      if (item.id === id) {
+        return { ...item, available_quantity: item.available_quantity + 1 };
+      }
+      return item;
+    });
+    setCartItems(updatedItems);
+  };
+
+  const handleDecreaseQuantity = () => {
+    const updatedItems = cartItems.map((item) => {
+      if (item.id === id) {
+        return item.available_quantity > 1
+          ? { ...item, available_quantity: item.available_quantity - 1 }
+          : item; // se quantidade for 1, não altera
+      }
+      return item;
+    }).filter(item => item.available_quantity > 0); // remove o item se a quantidade for 0
+
     setCartItems(updatedItems);
   };
 
@@ -26,6 +49,24 @@ function CartItem({ data }) {
       <div className="p-[0 35px 0 10px]">
         <h3 className="text-sm font-medium text-black/50 mb-2">{title}</h3>
         <h3 className="text-2xl font-medium">{formatCurrency(price, 'BRL')}</h3>
+
+        <div className="flex items-center mt-2">
+          <button
+            type="button"
+            className="text-[#d83232] text-xl border-none bg-none cursor-pointer mr-2"
+            onClick={ handleDecreaseQuantity }
+          >
+            <BsDashCircleFill />
+          </button>
+          <span className="text-lg font-medium">{available_quantity}</span>
+          <button
+            type="button"
+            className="text-[#32d832] text-xl border-none bg-none cursor-pointer ml-2"
+            onClick={ handleIncreaseQuantity }
+          >
+            <BsPlusCircleFill />
+          </button>
+        </div>
 
         <button
           type="button"
